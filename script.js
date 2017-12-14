f06ed6af32b9505f20fbcf4528a6baba68eaa584
dodi-dodi <dorota.durlej@gmail.com>
2017-12-14 13:41:21 +0100

    show navbar on mobile

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-jQuery(document).ready(function() {
-    jQuery('.toggle-nav').click(function(e) {
-        jQuery(this).toggleClass('active');
-        jQuery('.menu ul').toggleClass('active');
+(function navbar() {
+    let navbarMobileMenuButtons = document.querySelectorAll('#menu-icon');
+    for (let navbarMobileMenuButton of navbarMobileMenuButtons) {
+        navbarMobileMenuButton.addEventListener('click', function (e) {
+            e.preventDefault();
 
-        e.preventDefault();
-    });
-});
+            let menu = document.querySelector(navbarMobileMenuButton.getAttribute('data-menu-id'));
+            toogleFlex(menu);
+
+        });
+    }
+})();
+
+
+function toogleFlex(element) {
+    if (getComputedStyle(element).display === 'none') {
+        element.style.display = 'flex';
+    } else {
+        element.style.display = null;
+    }
+}
